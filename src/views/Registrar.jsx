ac7d693f2c856184ebac2ccb7ab6080d4c2fdb12
tamiaguilera <tami.aguilera@gmail.com>
2022-12-12 16:18:39 -0300

    se agrega react-hook-form

--- src/views/Registrar.jsx
+++ src/views/Registrar.jsx
@@ -1,23 +1,29 @@
+import { useForm } from "react-hook-form";
+
 const Registrar = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (e) => {
+        console.log(e);
+    };
     return(
     <div>
-        <form className="registrar">
+        <form className="registrar" onSubmit={handleSubmit(onSubmit)}>
 
             <h2 className="d-flex justify-content-center">Registro</h2>
             <div className="row d-flex justify-content-center">
                 <div className="col-12 mb-3">
-                    <input type="text" className="form-control" placeholder="Nombre" />
+                    <input type="text" {...register("nombre", {required: true})} className="form-control" placeholder="Nombre"  />
                 </div>
                 <div className="col-12 mb-3">
-                    <input type="text" className="form-control" placeholder="Correo"/>
+                    <input type="text" {...register("correo", {required: true})}   className="form-control" placeholder="Correo"/>
                 </div>
                 <div className="col-12 mb-3">
-                    <input type="text" className="form-control" placeholder="Contraseña" />
+                    <input type="text" {...register("constraseña1", {required: true})} className="form-control" placeholder="Contraseña" />
                 </div>
                 <div className="col-12 mb-3">
-                    <input type="text" className="form-control" placeholder="Repetir Contraseña" />
+                    <input type="text"  {...register("contraseña2", {required: true})} className="form-control" placeholder="Repetir Contraseña" />
                 </div>
-                <button className="btn btn-primary">Regístrate</button>
+                <input type="submit" value="Registrar" className="btn btn-primary" />
             </div>
         </form>
     </div>
