4b383a6557056f77c6b96ba2ffeff67c44793aa8
luciano tromo <lucianotromo@gmail.com>
2022-12-14 00:28:38 -0300

    cambios de fav

--- src/components/FavoritiesList.jsx
+++ src/components/FavoritiesList.jsx
@@ -5,28 +5,20 @@ import Context from "../context/context.js";
 import { formatPrice } from "../utils/utils.js";
 
 const Menu = () => {
-  const { menu, addToCart, favorites, addFavorities } = useContext(Context);
+  const { menu, favorites, addToCart } = useContext(Context);
   const navigate = useNavigate();
 
+  const favoritiesList = menu.filter((item) => favorites.includes(item.id));
   const viewProducto = (id) => navigate(`/alimento/${id}`);
 
   return (
     <section className="menu ">
-      {menu.map((item) => {
+      {favoritiesList.map((item) => {
         return (
           <div className="card" key={item.id}>
             <div className="content">
-              <div className="corazon">
-                <i
-                  onClick={() => {
-                    addFavorities(item.id);
-                  }}
-                  className={`${
-                    favorites.includes(item.id) ? "fa-solid" : "fa-regular"
-                  } fa-heart`}
-                ></i>
-              </div>
               <img src={item.img} alt={item.name} />
+
               <h4>{item.name}</h4>
               <h3 className="price">$ {formatPrice(item.price)}</h3>
 
--- src/views/Favoritos.jsx
+++ src/views/Favoritos.jsx
@@ -1,9 +1,26 @@
-const Favoritos = ()=> {
-    return (
-        <div className="vista-favoritos">
-            <h1>Tus Productos Favoritos</h1>
-        </div>
-    )
-}
+import { useNavigate } from "react-router-dom";
+import Carrusel from "../components/Carrusel.jsx";
+import FavoritiesList from "../components/FavoritiesList";
+import Header from "../components/Header.jsx";
 
-export default Favoritos
+const Favoritos = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  return (
+    <div>
+      <Carrusel />
+      <Header></Header>
+
+      <main>
+        <button onClick={() => logout()}>Logout</button>
+        <FavoritiesList />
+      </main>
+    </div>
+  );
+};
+
+export default Favoritos;
